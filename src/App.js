0276d988b9e0f23cf562bc7573609975a383cae2
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-05-24 08:09:03 +0200

    Feat: Seeing the map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import GoogleMap from './components/GoogleMap'
 
-const App = () => (
-  <div>
-    <h1>Hello, World!</h1>
-  </div>
-)
+const Wrapper = styled.main`
+  width: 100%;
+  height: 98vh;
+`
+
+const App = () => {
+  const [, setmapApiLoaded] = useState(false)
+  const [, setmapInstance] = useState()
+  const [, setmapApi] = useState()
+
+  const apiHasLoaded = (map, maps) => {
+    setmapApiLoaded(true)
+    setmapInstance(map)
+    setmapApi(maps)
+  }
+  return (
+    <Wrapper>
+      <GoogleMap
+        defaultCenter={{ lat: 40.37, lng: -5.0 }}
+        defaultZoom={6}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => apiHasLoaded(map, maps)}
+      />
+    </Wrapper>
+  )
+}
 
 export default App
